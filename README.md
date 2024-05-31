30473cae0d7d701b6a8f44e357a942181f08f33e
Jonathan Shapiro <shapiroj18@gmail.com>
2024-05-31 00:37:26 -0400

    configure tailwind, update readme

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ A little React Tutorial with Flowers
 ```
 yarn parcel src/index.html
 ```
+
+## Tooling
+* Uses [`parceljs`](https://parceljs.org/) as a simple web framework
+* Uses [`react`](https://react.dev/) for building UI
+* Uses [`tailwindcss`](https://tailwindcss.com/) as a css framework
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 export function Header() {
   return (
     <header>
-      <h1>Little Flowers</h1>
-      <p>A little React tutorial</p>
+      <h1 className="flex justify-center text-6xl">Little Flowers</h1>
+      <p className="flex justify-center">A little React tutorial</p>
     </header>
   );
 }
